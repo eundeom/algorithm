50665a996825005a3f9655cc0f5b4196da01c4b0
@eundeom <121565744+eundeom@users.noreply.github.com>
2024-10-04 21:57:14 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.11 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
+++ 프로그래머스/0/120809. 배열 두배 만들기/배열 두배 만들기.py
@@ -1,7 +1,7 @@
 def solution(numbers):
-    answer = [0 for _ in range(len(numbers))]
+    return [num*2 for num in numbers]
 
-    for i in range(len(numbers)):
-        answer[i] = numbers[i] * 2
-        
-    return answer
+    # answer = [0 for _ in range(len(numbers))]
+    # for i in range(len(numbers)):
+    #     answer[i] = numbers[i] * 2
+    # return answer
