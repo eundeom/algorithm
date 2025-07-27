7d99820a1aae73aa291899f66d952be6ce728860
@eundeom <121565744+eundeom@users.noreply.github.com>
2025-07-27 16:29:13 +0900

    [Silver V] Title: 숫자 카드, Time: 1196 ms, Memory: 125200 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,6 +1,5 @@
 import java.util.*;
 import java.io.*;
-import java.io.IOException;
 
 
 public class Main {
@@ -8,30 +7,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine()); // 상근이가 가지고 있는 카드 개수 N
-        ArrayList<Integer> arr = new ArrayList<>();
+        int[] arr = new int[N];
         String[] input = br.readLine().split(" "); // 상근이가 가지고 있는 카드의 숫자
         for(int i=0; i<N; i++){
-            arr.add(Integer.parseInt(input[i]));
+            arr[i] = (Integer.parseInt(input[i]));
         }
-        Collections.sort(arr);
+        Arrays.sort(arr);
 
         int M = Integer.parseInt(br.readLine()); // 상근이가 가진 카드인지 아닌지를 구분해야할 카드 M개
-        ArrayList<Integer> result = new ArrayList<>(); // 결과 저장할 배열
+        StringBuilder result = new StringBuilder(); // 결과 저장할 배열
         String[] comp = br.readLine().split(" "); // 비교해야할 카드들 입력받기
         for(int i=0; i<M; i++){
             int cmp = Integer.parseInt(comp[i]);
-            if(isInThere(cmp, arr)){
-                // 만약에 있으면 true이면 result에 add 1
-                result.add(1);
+            if(Arrays.binarySearch(arr, cmp) >= 0){
+                // java 내부적으로 최적화된 이진탐색 메서드 :: 해당 arr에서 key가 존재하면 index, 없으면 음수 반환
+                result.append("1 ");
             } else{
-                result.add(0);
+                result.append("0 ");
             }
         }
-        for(int r : result){
-            System.out.print(r + " ");
-        }
-
+        System.out.print(result);
     }
+
     // 상근이가 가진 수인지 확인할 숫자 cmp, 상근이가 가진 수의 배열 arr, mid
     public static boolean isInThere(int cmp, ArrayList<Integer> arr){
         int left = 0, right = arr.size()-1, mid = (left + right)/2;
